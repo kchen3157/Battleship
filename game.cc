4395c5a9e59700949a6febdcf11a35cdeb867ccc
Kyle Chen <kyle.chen111@outlook.com>
2023-11-28 14:10:36 -0800

    add main game loop

--- game.cc
+++ game.cc
@@ -16,6 +16,27 @@ int main()
     // Retrieve starting board from players
     get_user_start_board(&board_p0, 0);
     get_user_start_board(&board_p1, 1);
+    
+    nocbreak();        
+
+    // Main game loop
+    while (true)
+    {
+        // Player 0 turn
+        printw("Player 0: Your turn. Press enter to continue.\n");
+        refresh();
+        getch();
+        clear();
+        refresh();
+
+
+        // Player 1 turn
+        printw("Player 1: Your turn. Press enter to continue.\n");
+        refresh();
+        getch();
+        clear();
+        refresh();
+    }
 
     endwin(); // End ncurses
 
